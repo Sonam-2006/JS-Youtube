2e3294c9ef559aae1aefda85442c479ac6191cf0
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-08 18:05:39 +0000

    dataTypes

---
+++ 01_basics/02_dataTpes.js
@@ -0,0 +1,26 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs , not browser
+
+// console.log(3+
+//     3);          // code readability should be higher
+// console.log("SOnam");
+
+
+
+let name ="Sonam"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string = " "
+//boolean = false /true
+// null = standalone value
+//undefined = jot assign value
+//Symbol = using unique
+
+//object 
+
+console.log(typeof null); //obeject
+
