20a64b890e4f36478a8ede9e667b767db31c2eab
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-09 04:39:41 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33"  //33
+// let score = "33"  //33
 // let score = "33bd"  //NaN
 // let score = NaN
 // let score = undefined
@@ -15,7 +15,7 @@ let score = "33"  //33
 
 
 
-let isLoggedIn = 1;  // true
+// let isLoggedIn = 1;  // true
 // let isLoggedIn = "";  // false 
 // let isLoggedIn = 0;  // false
 // let isLoggedIn = "Sonam" // true
@@ -24,9 +24,49 @@ let isLoggedIn = 1;  // true
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************************************OPERATIONS *************************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2^3);
+
+// let str1 = "hello"
+// let str2 = "Sonam"
+
+// let str3 = str1+str2
+// console.log(str3); //hello Sonam
+
+// console.log('1' +2); //12
+// console.log(1 + "2");  //12
+// console.log("1" +2 +2); //122
+// console.log(1 + 2+ "2"); //32
+
+// console.log(3+4*5%2);
+
+// console.log(+true); // 1
+// console.log(+"");  // 0
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 =2+2
+
+// let gameCounter= 100
+// gameCounter++;
+// console.log(gameCounter); //101
+
+
+
+
 
