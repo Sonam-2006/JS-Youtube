acd6c0472fa1f5bcda81006b45dd160fca100b5e
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-10 05:32:18 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+// const score = 200
+// console.log(score); // 200
+
+// const balance = new Number(200)
+// console.log(balance);   //[Number: 200]
+
+// console.log(balance.toString().length);  //3
+// console.log(balance.toFixed(2));  //200.00
+
+// const otherNumber = 23.3862
+// console.log(otherNumber.toPrecision(3));  //23.4
+
+// const hunders = 1000000
+// console.log(hunders.toLocaleString('en-IN'));    //10,00,000
+
+//******************************************MATHS***********************************/
+
+// console.log(Math);    //Object [Math] {}
+// console.log(Math.abs(4));   //4
+// console.log(Math.round(4.6));    //5
+// console.log(Math.ceil(4.2));   //5
+// console.log(Math.floor(4.9));  //4
+// console.log(Math.sqrt(2.3));   //1.51657508881031
+// console.log(Math.min(6,22,8,1,3,81)); //1
+// console.log(Math.max(6,22,8,1,3,32));  //32
+
+console.log(Math.random());    //0.5614954177725333
+console.log((Math.random()*10)+1);   //10.93596382189232
+  const min = 10
+  const max = 20
+
+ console.log(Math.floor(Math.random()*(max-min+1))+min);  //12,16,19
+  
+
+
+
+
+
+
+
+
