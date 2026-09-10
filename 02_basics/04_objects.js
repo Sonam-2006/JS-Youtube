5ed57131fdbc979190311f09faad2cc992310196
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-10 19:02:38 +0000

    json api bascis

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,46 +1,69 @@
 //singleton
 // const tinderUser = new Object()
 //non-singleton
-const tinderUser2 ={}
+// const tinderUser2 ={}
 
-tinderUser2.id ='1234'
-tinderUser2.name="sonam"
-tinderUser2.isLoggedIn=false
-console.log(tinderUser2);  //{id: '1234', name: 'sonam', isLoggedIn: false}
+// tinderUser2.id ='1234'
+// tinderUser2.name="sonam"
+// tinderUser2.isLoggedIn=false
+// console.log(tinderUser2);  //{id: '1234', name: 'sonam', isLoggedIn: false}
 
-const regularUser ={
-    email:"some@gmail.com",
-    fullname:{
-        userfullname :{
-            firstname:"sonam",
-        lastname:"yadav"
-        }
-    }
-}
+// const regularUser ={
+//     email:"some@gmail.com",
+//     fullname:{
+//         userfullname :{
+//             firstname:"sonam",
+//         lastname:"yadav"
+//         }
+//     }
+// }
 
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 ={1:"a",2:"b"}
-const obj2 ={3:"a",4:"b"}
+// const obj1 ={1:"a",2:"b"}
+// const obj2 ={3:"a",4:"b"}
 
-// const obj3 ={obj1,obj2}
-// const  obj3 = Object.assign({}, obj1 ,obj2)
+// // const obj3 ={obj1,obj2}
+// // const  obj3 = Object.assign({}, obj1 ,obj2)
 
-const obj3 ={...obj1,...obj2}
-console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// const obj3 ={...obj1,...obj2}
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
-const users =[
-    {
-    id :1,
-    email:"sonam@gpt"
-}
+// const users =[
+//     {
+//     id :1,
+//     email:"sonam@gpt"
+// }
+// ]
+
+// users[1].email
+// console.log(tinderUser2);
+// console.log(Object.keys(tinderUser2));
+// console.log(Object.values(tinderUser2));
+// console.log(Object.entries(tinderUser2));
+
+// console.log(Object.hasOwnProperty('isLoggedIn'));
+
+// //distrueing 
+// const courese ={
+//     coursename:"js in hindi",
+//     price:"999",
+//     courseInstructor:"hitesh "
+// }
+
+// // courese.courseInstructor
+// const {courseInstructor: instructor}= courese
+// console.log(courseInstructor); //hitesh
+
+
+// const navbar=({company})=>{
+
+// }
+// navbar(company="hitesh")
+
+//--------------------api-----------------------
+[
+    {},
+    {},
+    {}
 ]
-
-users[1].email
-console.log(tinderUser2);
-console.log(Object.keys(tinderUser2));
-console.log(Object.values(tinderUser2));
-console.log(Object.entries(tinderUser2));
-
-console.log(Object.hasOwnProperty('isLoggedIn'));
-
