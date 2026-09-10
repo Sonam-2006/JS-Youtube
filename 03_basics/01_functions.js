ae3e28bc5b29d8097c0b92205d9aa1d17a68d3dd
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-10 19:08:35 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName (){
+    console.log("s");
+    console.log("o");
+    console.log("n");
+    console.log("a");
+    console.log("m");
+    
+}
+// sayMyName()
+
+function addTwoNum(num1, num2){
+    console.log(num1 + num2);
+    
+}
+addTwoNum(2,8) //10
