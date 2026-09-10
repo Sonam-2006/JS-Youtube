3ecc9a24d345546dced8d1ceb78d53a2a17af777
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-10 05:10:03 +0000

    add

---
+++ 06_nums_and_math.js
@@ -0,0 +1,13 @@
+const score = 200
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.3862
+console.log(otherNumber.toPrecision(3));
+
+
