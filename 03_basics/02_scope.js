53e26a62a12af6bca7de8a361c7f7adf7ad119e0
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-11 08:39:24 +0000

    add more detail in scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,11 +5,11 @@ if(true){ //block scope
 let a =10
 const b = 20
 // var c = 30
-console.log("Inner :",a); //10
+// console.log("Inner :",a); //10
 
 //scope node me alg hai aur inspect me alg hota h 
 }
-console.log(a); //300
+// console.log(a); //300
 // console.log(b);
 // console.log(c);
 
@@ -18,3 +18,47 @@ console.log(a); //300
     
 // }
 
+
+
+// ---------------------------mested--------------------------
+
+function one(){
+    const username ="sonam"
+    function two(){
+        const website = 'youtube'
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username ="sonam"
+    if(username === "sonam"){
+        const website ="youtube"
+        console.log(username+website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username); // its not scope
+
+
+
+//++++++++++++++++++++++++++++++++++ intersting ++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5)) // aise karne me error nhi ayega lakin 
+
+function addone (num){
+  return num +1
+}
+
+
+const addTwo= function(num){ // isko upar host karne me error ayega q ko function ko declaration ke sath variable me rakhe h 
+    return num +2
+}
+addTwo(5)
