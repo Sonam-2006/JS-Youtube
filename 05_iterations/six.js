81bb98ab7962310f9ebb3a247f66a56ede187254
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-13 17:14:25 +0000

    filter and forEach

---
+++ 05_iterations/six.js
@@ -0,0 +1,30 @@
+const Programming =['js','rb','py','java','c++']
+
+const values = Programming.forEach((item)=>{
+    // console.log(item);
+    // return item
+})
+// foreach koi bhi value ko return nhi karta hai
+// console.log(values);
+
+//filter me har ak no ko acess karta h lakin usme ham dete h condition ko
+const num = [1,2,3,4,5,6,7,8,9]
+
+// const newNum = num.filter((num)=> num>4)
+// console.log(newNum);
+
+// const newNum = num.filter((num)=> {
+//     return num>4
+// })
+// console.log(newNum);
+
+
+// const newNum = []
+// num.forEach((num)=>{
+//     if(num >4){
+//         newNum.push(num)
+//     }
+// })
+// console.log(newNum);
+
+
