9678bd18278e210d66666a851faffef6ac863af6
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-13 06:18:58 +0000

    map

---
+++ 05_iterations/three.js
@@ -0,0 +1,43 @@
+// for of 
+
+// ["","",""]
+// [{},{},{}]
+
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+// console.log(num);
+    
+}
+
+const greetings = "hello world!"
+for(const greet of greetings){
+    // console.log(`Each char is ${greet}`);
+    
+}
+
+
+//Maps 
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United ststes of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+// console.log(map);
+
+// for(const [key ,value] of map){
+//     console.log(key, ':' , value);
+    
+// }
+
+
+const myObj = {
+    'game':'NFS',
+    'game2' : 'Spiderman'
+}
+for(const [key , value] of myObj){
+    console.log(key ,':',value);
+    
+} 
