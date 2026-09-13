cdc8c06a4436923aab4a0af2519b6f30f4195c38
Sonam-2006 <sonamyadav2006mjg@gmail.com>
2026-09-13 06:41:26 +0000

    forEach

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+// for each
+const coding =['js','rb','py','java','c++']
+coding.forEach( function (item){
+    // console.log(item);
+    
+})
+
+
+// coding.forEach((val)=>{
+// console.log(val);
+
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+ // coding.forEach(printMe)
+
+
+ coding.forEach((item, index,arr)=>{
+    // console.log(item,index,arr);
+ })
+
+
+ const myCoding =[
+    {
+        languageNamee:'javaScript',
+        languageFileName : 'javascipt'
+    },
+      {
+        languageNamee:'python',
+        languageFileName : 'python'
+    },
+      {
+        languageNamee:'java',
+        languageFileName : 'java'
+    }
+ ]
+
+ myCoding.forEach((vla)=>{
+ console.log(vla.languageNamee);
+ 
+ })
